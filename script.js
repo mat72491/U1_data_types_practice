657680faeccb7137f3725c6b1c1e5fc737cb8d0e
mat72491 <michael.tibbetts.1@gmail.com>
2024-09-19 14:18:57 -0400

    completed and ready for submission

--- script.js
+++ script.js
@@ -4,13 +4,18 @@
  * Declare a variable called myFavoriteSong on one line and console.log the value
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
-
+const myFavoriteSong = 'The Boys of Summer'
+//console.log(myFavoriteSong)
+console.log(`My favorite song is ${myFavoriteSong}`)
 /*
  * Prompt 2:
  *
  * Create 3 variables using let and const, then console.log each of them, using a string literal (` ${} `} in the log
  */
-
+let firstName = 'Michael'
+let hometown = 'Yaphank'
+let age = 33
+//console.log(`Hi, my name is ${firstName}. I am ${age} years old. I live in ${hometown}. `)
 /*
  * Prompt 3:
  *
@@ -21,7 +26,12 @@
  *   - divide your variable by 7
  *   - calculate the remainder of dividing your variable by 2
  */
-
+let number = 13
+console.log(number + 10)
+console.log(number - 8)
+console.log(number * 3)
+console.log(number / 7)
+console.log(number % 2)
 /*
  * Prompt 4:
  *
@@ -33,13 +43,22 @@
  * and lastName variables.
  * Run a console.log that says 'hello my name is..." with your fullName. 
  */
-
+firstName = 'Michael'
+lastName = 'Tibbetts'
+fullName = (firstName + " " + lastName)
+console.log(`Hello my name is ${fullName}`)
 
 /* Arrays */
 
 // Create an array of Movies, then log the 2nd movie in your array
 // Use your array methods to add a movie in to the Front of the array, then the Back of the Array, then to the 2nd index point of the array
+let movies = ['The Sound of Music', 'Avengers: Endgame', 'Star Wars']
+console.log(movies[1])
 
+movies.unshift('The Day After Tomorrow')
+movies.push('The Wizard of Oz')
+movies.splice(1, 0, 'Balto') //found splice function on Google
+console.log(movies)
 
 /*
  *
@@ -56,14 +75,16 @@ let array3 = [
   ['Marty Feldman', ['Marty Marion']],
   ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
 ];
-
+console.log(array1[2])
+console.log(array2[1][1])
+console.log(array3[1][1][1])
 /*
  *
  * Use the length of the array below to retrieve the second to last item.
  */
 
 let array4 = ['a', 'b', 'c', 'd', 'e'];
-
+console.log(array4[array4.length - 2])
 /*
  *
  * Use the following arrays to answer the subprompts below.
@@ -77,9 +98,24 @@ let cathleen = ['Cathleen', 186, 'New York'];
 // Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
 // the thom array and replace it with "Tom".
 
+thom.shift()
+thom.unshift('Tom')
+console.log(thom)
+
+
 // Karolin just had her birthday; change Karolin's array to reflect her being
 // a year older.
 
+karolin.splice(1, 1, 17)
+console.log(karolin)
+
 // Change Cathleen's hometown from New York to "Gotham City".
 
+cathleen.pop()
+cathleen.push('Gotham City')
+console.log(cathleen)
+
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+ kristyn.pop()
+ kristyn.push('Oakland')
+ console.log(kristyn)
